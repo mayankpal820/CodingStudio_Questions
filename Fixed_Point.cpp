a7f39fbb97b21be00d2380d5dd59da60ac53edcd
mayankpal820 <mayankpal8202@gmail.com>
2023-12-23 12:24:03 +0530

    FixedPoint

---
+++ Fixed_Point.cpp
@@ -0,0 +1,10 @@
+//Code need to be write inside a given method name
+
+// int no=-1 ;
+// 	for(int i = 0 ; i <n ; i++){
+// 		if(i==arr[i]){
+// 			no = i;
+// 			break;
+// 		}
+// 	}
+// 	return no;
