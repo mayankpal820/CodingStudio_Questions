a15c4d195b1f15930eea949cc6374af35e89cf57
mayankpal820 <mayankpal8202@gmail.com>
2023-12-25 18:06:27 +0530

    Remove Duplicates From Sorted Array

---
+++ Arrays Easy/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+// Code need to be write inside a function
+// vector<int> newarray;
+// 	int i = 0 ;
+// 	while(i<=(n-1)){
+// 		if(arr[i]<arr[i+1]){
+			
+// 			newarray.push_back(arr[i]);
+// 		}
+// 		else if (i==(n-1)){
+// 			newarray.push_back(arr[i]);
+// 		}
+// 		i++;
+// 	}
+// 	int finalresult = newarray.size();
+// 	return finalresult;
