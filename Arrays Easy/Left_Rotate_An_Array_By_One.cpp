d9dd79d38547db66ecc37e65d2afc0d77b64531a
mayankpal820 <mayankpal8202@gmail.com>
2023-12-26 04:18:29 +0530

    Left Rotated an Array by One

---
+++ Arrays Easy/Left_Rotate_An_Array_By_One.cpp
@@ -0,0 +1,17 @@
+// Code eed to be write inside a function
+// vector<int> newarray;
+//     int i = 0 ;
+//     int firstelement = arr[i];
+//     int restelements;
+//     while(i<=(n-1)){
+//         if(i<n-1){
+//             restelements=arr[i+1];
+
+//         }
+//         else{
+//             restelements = firstelement;
+//         }
+//         newarray.push_back(restelements);
+//         i++;
+//     }
+//     return newarray;
