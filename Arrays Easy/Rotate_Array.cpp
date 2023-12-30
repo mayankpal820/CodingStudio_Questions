57296860d9bcc7815205595f4d7d3bb2f97d91ab
mayankpal820 <mayankpal8202@gmail.com>
2023-12-30 12:46:39 +0530

    Rotate Array

---
+++ Arrays Easy/Rotate_Array.cpp
@@ -0,0 +1,51 @@
+// Code need to be write inside a function 
+
+/*
+vector<int> newvec;
+    
+    int i = k;
+    int loop = 0;
+    if(arr.size()==1){
+        
+        newvec.push_back(arr[0]);
+
+    }
+    if(arr.size()==2){
+        if(k==1){
+            newvec.push_back(arr[1]);
+           newvec.push_back(arr[0]);
+        }
+        else{
+            newvec.push_back(arr[0]);
+        newvec.push_back(arr[1]);
+        }
+        return newvec;
+    }
+    
+    while(i<=(arr.size()-1) && loop<=1){
+
+        if(i>(k-1) && loop==0){
+            
+             newvec.push_back(arr[i]);
+                i++; 
+            if(i>(arr.size()-1)){
+                i = 0 ;
+                loop = loop + 1;
+            }
+            
+               
+            
+        }
+        
+        else{
+            newvec.push_back(arr[i]);
+            i++;
+            if(i>=k){
+                loop = loop + 1 ;
+            }
+            
+        }                                                                                                                                                                 
+       
+    }
+    
+    return newvec;*/
