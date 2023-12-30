280808216731d51d87babf5d09136d5ad00264cd
mayankpal820 <mayankpal8202@gmail.com>
2023-12-30 18:02:44 +0530

    Linear Search

---
+++ Arrays Easy/Linear_Search.cpp
@@ -0,0 +1,17 @@
+// Code need to be write inside a function
+/*
+ Write your code here.
+    int i = 0 ;
+    int result ;
+    while(i<=(n-1)){
+        if(arr[i]==num){
+            result = i;
+            break;
+        }
+        else{
+            i++;
+            result = -1;
+        }
+    }
+    return result;
+    */
